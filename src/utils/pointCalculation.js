9affd28b98477342a251ea105bb4a9e737097331
samposihvola <fytapes@proton.me>
2024-10-24 12:04:50 +0300

    Update pointCalculation.js

--- src/utils/pointCalculation.js
+++ src/utils/pointCalculation.js
@@ -1,12 +1,12 @@
 let points = 0;
 
 const pointCalculation = (number) => {
-	if (number > 0) {
-		points += number;
-	} if (number === 0)  {
-		points = 0
-	}
-	return points;
+  if (number > 0) {
+    points += number;
+  } if (number === 0)  {
+    points = 0
+  }
+  return points;
 };
 
 export default pointCalculation;
