b8db376f03c1ca20a25d4b33c555f8709a484ba7
samposihvola <fytapes@proton.me>
2024-10-24 12:10:43 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import pointCalculation from '../utils/pointCalculation';
 
 const Header = () => {
-	const points = pointCalculation();
-	return (
-		<div>
-			<h1>2048</h1>
-			<div className="points">Points {points}</div>
-		</div>
-	);
+  const points = pointCalculation();
+  return (
+    <div>
+      <h1>2048</h1>
+      <div className="points">Points {points}</div>
+    </div>
+  );
 };
 
 export default Header;
