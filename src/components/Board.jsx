34535eb525ba0fadb2af49517319ca20c33569e3
samposihvola <fytapes@proton.me>
2024-10-24 12:10:22 +0300

    Update Board.jsx

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,21 +2,21 @@ import Block from "./Block";
 
 const Board = ({ gameGrid }) => {
   return (
-		<div className="gameGrid">
-			{/* Mapping over each row in the array */}
-			{gameGrid.map((row, rowIndex) => {
-				return (
-					<div className="gameRow" key={rowIndex}>
-						{/* Mapping over each digit in the current row */}
-						{row.map((digit, columnIndex) => (
-							// Rendering a Block component for each digit in the row
-							<Block key={columnIndex} digit={digit === 0 ? null : digit} />
-						))}
-					</div>
-				);
-			})}
-		</div>
-	);
+    <div className="gameGrid">
+      {/* Mapping over each row in the array */}
+      {gameGrid.map((row, rowIndex) => {
+        return (
+          <div className="gameRow" key={rowIndex}>
+            {/* Mapping over each digit in the current row */}
+            {row.map((digit, columnIndex) => (
+              // Rendering a Block component for each digit in the row
+              <Block key={columnIndex} digit={digit === 0 ? null : digit} />
+            ))}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Board;
